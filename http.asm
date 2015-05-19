76a0cc0595790e3658e7eeb9572cd79b13f737cd
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-05-19 15:28:50 -0700

    get it building on osx

--- http.asm
+++ http.asm
@@ -20,61 +20,61 @@
 detect_content_type: ;rdi - pointer to buffer that contains request, ret - rax: type flag
 	stackpush
 
-	mov rsi, extension_htm
+	mov rsi, qword extension_htm
 	call string_ends_with
 	mov r10, CONTENT_TYPE_HTML
 	cmp rax, 1
 	je detect_content_type_ret
 
-	mov rsi, extension_html
+	mov rsi, qword extension_html
 	call string_ends_with
 	mov r10, CONTENT_TYPE_HTML 
 	cmp rax, 1
 	je detect_content_type_ret
 	
-	mov rsi, extension_css
+	mov rsi, qword extension_css
 	call string_ends_with
 	mov r10, CONTENT_TYPE_CSS
 	cmp rax, 1
 	je detect_content_type_ret
 	
-	mov rsi, extension_javascript
+	mov rsi, qword extension_javascript
 	call string_ends_with
 	mov r10, CONTENT_TYPE_JAVASCRIPT
 	cmp rax, 1
 	je detect_content_type_ret
 
-	mov rsi, extension_xhtml
+	mov rsi, qword extension_xhtml
 	call string_ends_with
 	mov r10, CONTENT_TYPE_XHTML
 	cmp rax, 1
 	je detect_content_type_ret
 
-	mov rsi, extension_xml
+	mov rsi, qword extension_xml
 	call string_ends_with
 	mov r10, CONTENT_TYPE_XML
 	cmp rax, 1
 	je detect_content_type_ret
 
-	mov rsi, extension_gif
+	mov rsi, qword extension_gif
 	call string_ends_with
 	mov r10, CONTENT_TYPE_GIF
 	cmp rax, 1
 	je detect_content_type_ret
 	
-	mov rsi, extension_png
+	mov rsi, qword extension_png
 	call string_ends_with
 	mov r10, CONTENT_TYPE_PNG
 	cmp rax, 1
 	je detect_content_type_ret
 	
-	mov rsi, extension_jpeg
+	mov rsi, qword extension_jpeg
 	call string_ends_with
 	mov r10, CONTENT_TYPE_JPEG
 	cmp rax, 1
 	je detect_content_type_ret
 	
-	mov rsi, extension_jpg
+	mov rsi, qword extension_jpg
 	call string_ends_with
 	mov r10, CONTENT_TYPE_JPEG
 	cmp rax, 1
@@ -91,7 +91,7 @@ add_content_type_header: ;rdi - pointer to buffer, rsi - type
 
 	mov r10, rsi
 
-	mov rsi, content_type
+	mov rsi, qword content_type
 	call string_concat
 
 	cmp r10, CONTENT_TYPE_HTML
@@ -116,47 +116,47 @@ add_content_type_header: ;rdi - pointer to buffer, rsi - type
 	jmp add_response_octet_stream
 
 	add_response_html:
-	mov rsi, content_type_html
+	mov rsi, qword content_type_html
 	call string_concat
 	jmp add_response_cont
 	
 	add_response_octet_stream:
-	mov rsi, content_type_octet_stream
+	mov rsi, qword content_type_octet_stream
 	call string_concat
 	jmp add_response_cont
 	
 	add_response_css:
-	mov rsi, content_type_css
+	mov rsi, qword content_type_css
 	call string_concat
 	jmp add_response_cont
 
 	add_response_javascript:
-	mov rsi, content_type_javascript
+	mov rsi, qword content_type_javascript
 	call string_concat
 	jmp add_response_cont
 
 	add_response_xhtml:
-	mov rsi, content_type_xhtml
+	mov rsi, qword content_type_xhtml
 	call string_concat
 	jmp add_response_cont
 
 	add_response_xml:
-	mov rsi, content_type_xml
+	mov rsi, qword content_type_xml
 	call string_concat
 	jmp add_response_cont
 
 	add_response_gif:
-	mov rsi, content_type_gif
+	mov rsi, qword content_type_gif
 	call string_concat
 	jmp add_response_cont
 
 	add_response_png:
-	mov rsi, content_type_png
+	mov rsi, qword content_type_png
 	call string_concat
 	jmp add_response_cont
 
 	add_response_jpeg:
-	mov rsi, content_type_jpeg
+	mov rsi, qword content_type_jpeg
 	call string_concat
 
 	add_response_cont:
@@ -172,31 +172,31 @@ create_httpError_response: ;rdi - pointer, rsi - error code: 400, 416, 413
 	je create_httpError_response_413
 	
 	;garbage/default is 400
-	mov rsi, http_400
+	mov rsi, qword http_400
 	mov rdx, http_400_len
 	call string_copy
 	jmp create_httpError_response_cont
 
 	create_httpError_response_416:
-	mov rsi, http_416
+	mov rsi, qword http_416
 	mov rdx, http_416_len
 	call string_copy
 	jmp create_httpError_response_cont
 
 	create_httpError_response_413:
- 	mov rsi, http_413
+	mov rsi, qword http_413
 	mov rdx, http_413_len
 	call string_copy
 	jmp create_httpError_response_cont
 	
 	create_httpError_response_cont:
-	mov rsi, server_header
+	mov rsi, qword server_header
 	call string_concat
 
-	mov rsi, connection_header
+	mov rsi, qword connection_header
 	call string_concat
 
-	mov rsi, crlfx2
+	mov rsi, qword crlfx2
     call string_concat
 
 	call get_string_length
@@ -213,23 +213,23 @@ create_http206_response: ;rdi - pointer, rsi - from, rdx - to, r10 - total r9 - 
 	push rsi
 	push rdx
 
-	mov rsi, http_206 ; copy first one
+	mov rsi, qword http_206 ; copy first one
 	mov rdx, http_206_len
 	call string_copy
 
-	mov rsi, server_header
+	mov rsi, qword server_header
 	call string_concat
 
-	mov rsi, connection_header
+	mov rsi, qword connection_header
 	call string_concat
 
-	mov rsi, range_header
+	mov rsi, qword range_header
 	call string_concat
 	
 	mov rsi, r9
 	call add_content_type_header
 
-	mov rsi, content_length
+	mov rsi, qword content_length
 	call string_concat
 	
 	pop rdx
@@ -243,28 +243,28 @@ create_http206_response: ;rdi - pointer, rsi - from, rdx - to, r10 - total r9 - 
 
 	call string_concat_int
 	
-	mov rsi, crlf
+	mov rsi, qword crlf
 	call string_concat
 	
-	mov rsi, content_range
+	mov rsi, qword content_range
 	call string_concat
 	
 	pop rsi
 	call string_concat_int
 
-	mov rsi, char_hyphen
+	mov rsi, qword char_hyphen
 	call string_concat
 
 	mov rsi, rdx
 	call string_concat_int
 
-	mov rsi, char_slash
+	mov rsi, qword char_slash
 	call string_concat
 
 	mov rsi, r10 ; val 
 	call string_concat_int
 	
-	mov rsi, crlfx2
+	mov rsi, qword crlfx2
 	call string_concat
 
 	call get_string_length
@@ -286,32 +286,32 @@ create_http200_response: ;rdi - pointer to buffer, rsi - type, rdx - length
 
 	mov r10, rsi ;type
 	
-	mov rsi, http_200  ;First one we copy
+	mov rsi, qword http_200  ;First one we copy
 	mov rdx, http_200_len
 	call string_copy
 
-	mov rsi, server_header
+	mov rsi, qword server_header
 	call string_concat
 
 	;mov rsi, connection_header
 	;call string_concat
 
-	mov rsi, range_header
+	mov rsi, qword range_header
 	call string_concat
 	
-	mov rsi, content_length
+	mov rsi, qword content_length
 	call string_concat
 
 	pop rsi ; length
 	call string_concat_int
 	
-	mov rsi, crlf
+	mov rsi, qword crlf
 	call string_concat
 
 	mov rsi, r10
 	call add_content_type_header
 	
-	mov rsi, crlf
+	mov rsi, qword crlf
 	call string_concat
 
 	call get_string_length
@@ -322,23 +322,23 @@ create_http200_response: ;rdi - pointer to buffer, rsi - type, rdx - length
 create_http404_response: ;rdi - pointer to buffer
 	stackpush
 
-	mov rsi, http_404  ;First one we copy
+	mov rsi, qword http_404  ;First one we copy
 	mov rdx, http_404_len
 	call string_copy
 
-	mov rsi, server_header
+	mov rsi, qword server_header
 	call string_concat
 
-	mov rsi, connection_header
+	mov rsi, qword connection_header
 	call string_concat
 
-	mov rsi, crlf
+	mov rsi, qword crlf
 	call string_concat
 
-	mov rsi, http_404_text
+	mov rsi, qword http_404_text
 	call string_concat
 
-	mov rsi, crlf
+	mov rsi, qword crlf
 	call string_concat
 
 	call get_string_length
